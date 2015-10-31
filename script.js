d20f1f3decf903e42eb3e1591b2ad3cefebe6d40
C.T. Lin <chentsulin@gmail.com>
2015-11-01 00:45:10 +0800

    other try

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ var svg = d3.select('body').append('svg')
     .attr('height', '600px')
     .attr('class', 'chart');
 
-svg.append('circle')
-  .attr('cx', 600 / 2)
-  .attr('cy', 600 / 2)
-  .attr('r', 600 / 2)
-  .style('fill', 'black')
-  .on('mouseover', handleMouseover);
+makeCircle(svg, {
+  'cx': 600 / 2,
+  'cy': 600 / 2,
+  'r': 600 / 2,
+  'color': 'black'
+});
+
+function makeCircle(svg, attr) {
+  var c = svg.append('circle');
+  c
+    .attr('cx', attr.cx)
+    .attr('cy', attr.cy)
+    .attr('r', 0)
+    .transition()
+    .duration(500)
+    .attr('r', attr.r)
+    .style('fill', attr.color);
+
+  c.on('mouseover', handleMouseover);
+}
 
 function handleMouseover() {
   var el = d3.select(this);
@@ -18,29 +32,35 @@ function handleMouseover() {
     r: +el.attr('r')
   };
 
-  svg.append('circle')
-    .attr('cx', original.cx + (original.r / 2))
-    .attr('cy', original.cy + (original.r / 2))
-    .attr('r', original.r / 2)
-    .style('fill', 'black')
-    .on('mouseover', handleMouseover);
-  svg.append('circle')
-    .attr('cx', original.cx + (original.r / 2))
-    .attr('cy', original.cy - (original.r / 2))
-    .attr('r', original.r / 2)
-    .style('fill', 'black')
-    .on('mouseover', handleMouseover);
-  svg.append('circle')
-    .attr('cx', original.cx - (original.r / 2))
-    .attr('cy', original.cy + (original.r / 2))
-    .attr('r', original.r / 2)
-    .style('fill', 'black')
-    .on('mouseover', handleMouseover);
-  el
-    .attr('cx', original.cx - (original.r / 2))
-    .attr('cy', original.cy - (original.r / 2))
-    .attr('r', (original.r / 2));
-}
+  el.remove();
 
+  makeCircle(svg, {
+    'cx': original.cx + (original.r / 2),
+    'cy': original.cy + (original.r / 2),
+    'r': original.r / 2,
+    'color': 'black'
+  });
+
+  makeCircle(svg, {
+    'cx': original.cx + (original.r / 2),
+    'cy': original.cy - (original.r / 2),
+    'r': original.r / 2,
+    'color': 'black'
+  });
+
+  makeCircle(svg, {
+    'cx': original.cx - (original.r / 2),
+    'cy': original.cy + (original.r / 2),
+    'r': original.r / 2,
+    'color': 'black'
+  });
+
+  makeCircle(svg, {
+    'cx': original.cx - (original.r / 2),
+    'cy': original.cy - (original.r / 2),
+    'r': original.r / 2,
+    'color': 'black'
+  });
+}
 
 
